127730eec9bbeb59d478b5c4801c068b9e715949
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2008-04-02 23:44:48 -0700

    Implemented all logic to decide between serving resized/unresized image or 404
    - Created stubs: (transfer-unresized-image) and (transfer-resized-image).
    - Created (resize-required?) to return the resized dimensions if a resize is
      required. If a resize is not required, nil is returned.
    - Renamed the functions for calculating the resized dimensions.
    - (img-url-handler) now allows requests without any GET parameters. If there
      are no parameters, the unresized image is returned.

--- imgdb-web/img-urls.lisp
+++ imgdb-web/img-urls.lisp
@@ -5,51 +5,74 @@
 (defun img-url-handler ()
   (let* ((img-id (get-img-id-from-uri (script-name)))
          (req-params (get-parameters))
-         (req-type (read-from-string (caar req-params)))
-         (req-arg (parse-integer (cdar req-params) :junk-allowed t)))
-    (if (and (= (length req-params) 1)
-             (not (null req-arg))
-             (subsetp (list req-type)
-                      '(resize width height)))
-        ;; Check whether image exists in imgdb and get image
-        ;; location in imgdb-store.
-        (let ((imgdb-store-url (get-imgdb-store-url-from-img-id img-id)))
-          (if (null imgdb-store-url)
-              (not-found-page)
-              (progn
-                ;; Read image and get dimensions.
-                ;; Get resized dimensions.
-                ;; Resize the image, if needed.
-                ;; Send the (resized?) image back.
-                ;; It doesn't look like there is any way in MagickWand
-                ;; of doing the reading, resizing and sending of the
-                ;; image using buffers (instead of loading the entire
-                ;; image into memory).
-                (format nil "still under construction"))))
-        (not-found-page))))
+         (req-type (when req-params (read-from-string (caar req-params))))
+         (req-arg (when req-params
+                    (parse-integer (cdar req-params) :junk-allowed t))))
+    (cond 
+        ((and (= (length req-params) 1)
+              (not (null req-arg))
+              (subsetp (list req-type)
+                       '(resize width height)))
+         (let ((img-store-url-and-size
+                (get-img-store-url-and-size-from-img-id img-id)))
+           (if (null img-store-url-and-size)
+               (not-found-page)
+               (let* ((img-store-url (first img-store-url-and-size))
+                      (img-width (second img-store-url-and-size))
+                      (img-height (third img-store-url-and-size))
+                      (resized-dimensions
+                       (resize-required? img-width img-height
+                                         req-type req-arg)))
+                 (if (not resized-dimensions)
+                     (transfer-unresized-image img-store-url)
+                     (transfer-resized-image img-store-url
+                                             (first resized-dimensions)
+                                             (second resized-dimensions)))))))
+      ((= (length req-params) 0)
+       (let ((img-store-url
+              (first (get-img-store-url-and-size-from-img-id img-id))))
+         (transfer-unresized-image img-store-url)))
+      (t (not-found-page)))))
 
-(defun get-resized-dimensions (width height req-type req-arg)
+(defun transfer-unresized-image (img-store-url)
+  ;; Just serve up the unaltered image
+  (format nil "still under construction"))
+(defun transfer-resized-image (img-store-url new-width new-height)
+  ;; Read and resize the image.
+  ;; Resize the image
+  ;; Send the resized image back.
+  ;; It doesn't look like there is any way in MagickWand
+  ;; of doing the reading, resizing and sending of the
+  ;; image using buffers (instead of loading the entire
+  ;; image into memory).
+  (format nil "still under construction"))
+
+(defun resize-required? (width height req-type req-arg)
   (ccase req-type
     (resize
-     (get-resized-dimensions-for-percentage-resize width height req-arg))
+     (and (not (= req-arg 100))
+          (resize-dimensions-by-percentage width height req-arg)))
     (width
-     (get-resized-dimensions-for-width-resize width height req-arg))
+     (and (not (= req-arg width))
+          (resize-dimensions-to-width width height req-arg)))
     (height
-     (get-resized-dimensions-for-height-resize width height req-arg))))
+     (and (not (= req-arg height))
+          (resize-dimensions-to-height width height req-arg)))))
 
-(defun get-resized-dimensions-for-percentage-resize (width height percent)
+(defun resize-dimensions-by-percentage (width height percent)
   (mapcar #'(lambda (x)
               (floor (* x (/ percent 100))))
           (list width height)))
 
-(defun get-resized-dimensions-for-width-resize (width height new-width)
+(defun resize-dimensions-to-width (width height new-width)
   (list new-width (floor (* height (/ new-width width)))))
 
-(defun get-resized-dimensions-for-height-resize (width height new-height)
+(defun resize-dimensions-to-height (width height new-height)
   (list (floor (* width (/ new-height height))) new-height))
 
-(defun get-imgdb-store-url-from-img-id (img-id)
-  (caar (select-img-records ([url]) [= [digest] img-id] *imgdb-dbconn*)))
+(defun get-img-store-url-and-size-from-img-id (img-id)
+  (car (select-img-records ([url] [width] [height])
+                           [= [digest] img-id] *imgdb-dbconn*)))
 
 (defun get-img-id-from-uri (uri)
   (register-groups-bind (img-id)
