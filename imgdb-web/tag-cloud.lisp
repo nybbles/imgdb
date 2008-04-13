c85237ce1f6590c112c745e4a88464d8b44ed430
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2008-04-12 21:45:40 -0700

    Tag cloud now uses variable bases dependent on average of quantities.
    - Cleaned up CSS so that different tag classes make a visual difference.
    - The base used to calculate the tag class of a tag is dependent on the average
      of the quantities and the number of tag classes. The base is such that the
      average of the quantities will belong in the middle tag class.

--- imgdb-web/tag-cloud.lisp
+++ imgdb-web/tag-cloud.lisp
@@ -1,22 +1,28 @@
 (in-package :imgdb-web)
 
 (defun generate-tag-cloud (tags)
-  (let ((mid-exponent
-         (get-middle-exponent (mapcar #'(lambda (x) (third x)) tags))))
+  (let* ((quantities (mapcar #'(lambda (x) (get-quantity x)) tags))
+         (base (get-covering-base quantities))
+         (mid-exponent
+          (get-middle-exponent quantities base)))
     (with-html-output-to-string (output nil)
       (loop for tag in tags
          do (let ((tag-name (first tag))
                   (link (second tag))
                   (quantity (third tag)))
               (htm
-               (:span :class (get-cloud-tag-class quantity mid-exponent)
+               (:span :class (get-cloud-tag-class quantity mid-exponent base)
                       (:a :href link
+                          :title 
+                          (let ((num-str (if (= quantity 0) "No" quantity))
+                                (imgs-str (if (= quantity 1) "image" "images")))
+                            (format nil "~A ~A" num-str imgs-str))
                           (str tag-name))) " "))))))
 
-(defun get-cloud-tag-class (quantity mid-exponent)
+(defun get-cloud-tag-class (quantity mid-exponent &optional (base 10))
   (concatenate 'string
                "M"
-               (write-to-string (- (floor (log quantity 10)) mid-exponent))))
+               (write-to-string (- (floor (log quantity base)) mid-exponent))))
 
 (defun get-middle-exponent (quantities &optional (base 10))
   (let ((expts (mapcar #'(lambda (x) (floor (log x base))) quantities)))
@@ -24,3 +30,10 @@
 
 (defun average (numbers)
   (/ (reduce #'+ numbers) (length numbers)))
+
+(defun get-covering-base (numbers &optional (coverage 5))
+  (assert (oddp coverage))
+  (expt (average numbers) (/ 1 (/ (- coverage 1) 2))))
+
+(defun get-quantity (tag)
+  (third tag))
