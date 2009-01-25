705ebed01879d1bd7b12c7d84fea9795f8437c5d
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2009-01-24 21:30:51 -0800

    Turned the image tag functions into methods of imgdb-store

--- imgdb-store/imgdb-tags.lisp
+++ imgdb-store/imgdb-tags.lisp
@@ -4,14 +4,22 @@
 
 (defparameter *max-tag-length* 64)
 
-(defun get-img-tags (img-id dbconn)
+(defmethod get-img-tags ((store imgdb-store) img-id)
+  (with-dbconn-info (dbconn (dbconn-info store))
+    (get-img-tags-with-dbconn store img-id dbconn)))
+
+(defmethod get-img-tags-with-dbconn ((store imgdb-store) img-id dbconn)
   (select [tag]
           :from *img-tags-table*
           :flatp t
           :where [= [imgid] img-id]
           :database dbconn))
 
-(defun add-img-tags (img-id tags dbconn)
+(defmethod add-img-tags ((store imgdb-store) img-id tags)
+  (with-dbconn-info (dbconn (dbconn-info store))
+    (add-img-tags-with-dbconn store img-id tags dbconn)))
+
+(defmethod add-img-tags-with-dbconn ((store imgdb-store) img-id tags dbconn)
   (with-transaction (:database dbconn)
     (let ((tags-to-add (set-difference tags (get-img-tags img-id dbconn))))
       (dolist (tag tags-to-add)
@@ -21,7 +29,11 @@
                         :database dbconn)))
     (get-img-tags img-id dbconn)))
 
-(defun delete-img-tags (img-id tags dbconn)
+(defmethod delete-img-tags ((store imgdb-store) img-id tags)
+  (with-dbconn-info (dbconn (dbconn-info store))
+    (delete-img-tags-with-dbconn store img-id tags dbconn)))
+
+(defmethod delete-img-tags-with-dbconn ((store imgdb-store) img-id tags dbconn)
   (with-transaction (:database dbconn)
     (delete-records :from *img-tags-table*
                     :where
@@ -29,21 +41,20 @@
                     :database dbconn)
     (get-img-tags img-id dbconn)))
 
-(defun create-img-tags-table (dbconn-spec db-type)
-  (with-database (dbconn dbconn-spec
-                         :database-type db-type :pool t :if-exists :old)
+(defmethod create-img-tags-table ((store imgdb-store))
+  (with-dbconn-info (dbconn (dbconn-info store))
     (create-table *img-tags-table*
                   '(([imgid] (vector char 40) :not-null)
                     ([tag] (vector char *max-tag-length*) :not-null))
                   :constraints '("PRIMARY KEY (imgid, tag)")
                   :database dbconn)))
-(defun drop-img-tags-table (dbconn-spec db-type)
-  (with-database (dbconn dbconn-spec
-                         :database-type db-type :pool t :if-exists :old)
+
+(defmethod drop-img-tags-table ((store imgdb-store))
+  (with-dbconn-info (dbconn (dbconn-info store))
     (drop-table *img-tags-table* :database dbconn)))
-(defun img-tags-table-exists (dbconn-spec db-type)
-  (with-database (dbconn dbconn-spec
-                         :database-type db-type :pool t :if-exists :old)
+
+(defmethod img-tags-table-exists ((store imgdb-store))
+  (with-dbconn-info (dbconn (dbconn-info store))
     (table-exists-p *img-tags-table* :database dbconn)))
 
 (restore-sql-reader-syntax-state)
