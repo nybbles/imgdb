8ede8b0cebb102aa1829684242c2ca1a684c754c
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2009-01-25 01:08:45 -0800

    Rewrote (setup-dispatch-table) as a method

--- imgdb-web/dispatchers-setup.lisp
+++ imgdb-web/dispatchers-setup.lisp
@@ -1,35 +1,45 @@
 (in-package :imgdb-web)
 
-(defun setup-dispatch-table ()  
-  (setf *dispatch-table* '())
-  (push (create-regex-dispatcher "/.*" 'not-found-page) *dispatch-table*)
-  (push (create-prefix-dispatcher "/index.htm" 'welcome-page) *dispatch-table*)
-  (push (create-regex-dispatcher "^/$" 'welcome-page) *dispatch-table*)
-  (push (create-regex-dispatcher "/img-urls/thumbnails/[a-f0-9]+$"
-                                 'img-thumbnail-handler)
-        *dispatch-table*)
-  (push (create-regex-dispatcher "/img-urls/[a-f0-9]+$" 'img-url-handler)
-        *dispatch-table*)
-  (push (create-regex-dispatcher "/img-view$" 'img-view-page)
-        *dispatch-table*)
-  (push (create-regex-dispatcher "/img-query$" 'img-query-page)
-        *dispatch-table*)
-  (push (create-folder-dispatcher-and-handler
-         "/js/"
-         (merge-pathnames "js/" *imgdb-web-root*))
-        *dispatch-table*)
-  (push (create-static-file-dispatcher-and-handler
-         "/css/imgdb-web.css"
-         (merge-pathnames "css/imgdb-web.css" *imgdb-web-root*))
-        *dispatch-table*)
-  (push (create-regex-dispatcher "/get-img-tags$" 'get-img-tags-handler)
-        *dispatch-table*)
-  (push (create-regex-dispatcher "/add-img-tags$" 'add-img-tags-handler)
-        *dispatch-table*)
-  (push (create-regex-dispatcher "/delete-img-tags$" 'delete-img-tags-handler)
-        *dispatch-table*)
-  (push (create-regex-dispatcher "/set-img-title$" 'set-img-title-handler)
-        *dispatch-table*)
-  (push (create-regex-dispatcher "/set-img-description$"
-                                 'set-img-description-handler)
-        *dispatch-table*))
+(defmethod setup-dispatch-table ((web imgdb-web-server))
+  (setf (dispatch-table web) '())
+  (mapcar
+   #'(lambda (dispatch)
+       (let ((dispatch-type (first dispatch))
+             (url (second dispatch))
+             (method (third dispatch)))
+         (let ((create-dispatcher-fn
+                (case dispatch-type
+                  (:regex #'create-regex-dispatcher)
+                  (:prefix #'create-prefix-dispatcher)
+                  (otherwise (error "Invalid dispatcher type"))))
+               (dispatcher-fn
+                (create-dispatcher-fn-for-method web method)))
+           (push
+            (funcall create-dispatcher-fn url dispatcher-fn)
+            (dispatch-table web)))))
+   '((:regex "/.*" 'not-found-page)
+     (:prefix "/index.htm" 'welcome-page)
+     (:regex "^/$" 'welcome-page)
+     (:regex "/img-urls/thumbnails/[a-f0-9]+$" 'img-thumbnail-handler)
+     (:regex "/img-urls/[a-f0-9]+$" 'img-url-handler)
+     (:regex "/img-view$" 'img-view-page)
+     (:regex "/img-query$" 'img-query-page)
+     (:regex "/get-img-tags$" 'get-img-tags-handler)
+     (:regex "/add-img-tags$" 'add-img-tags-handler)
+     (:regex "/delete-img-tags$" 'delete-img-tags-handler)
+     (:regex "/set-img-title$" 'set-img-title-handler)
+     (:regex "/set-img-description$" 'set-img-description-handler)))
+  (push
+   (create-folder-dispatcher-and-handler
+    "/js/"
+    (merge-pathnames "js/" (web-root web)))
+   (dispatch-table web))
+  (push
+   (create-static-file-dispatcher-and-handler
+    "/css/imgdb-web.css"
+    (merge-pathnames "css/imgdb-web.css" (web-root web)))
+   (dispatch-table web)))
+
+(defmethod create-dispatcher-fn-for-method
+    ((web imgdb-web-server) dispatch-method)
+  #'(lambda () (funcall dispatch-method web)))
