c639fa43d36cf973fec9dd9eab79b42b8e509b77
Nimalan Mahendran <nmahendran@alumni.uwaterloo.ca>
2009-01-25 15:10:13 -0800

    Dispatch table is now handled properly by imgdb-web server
    - The dispatch table is contained in the server handle, not in the imgdb-web
      server itself. Previously, updates to the imgdb-web server were done on a
      dispatch table contained within the imgdb-web server itself. This did not
      result in the actual dispatch table getting updated. The dispatch table slot
      in the imgdb-web server is now a virtual slot where the dispatch-table slot
      of the underlying server handle is actually get/set-ed.

--- imgdb-web/imgdb-web-server.lisp
+++ imgdb-web/imgdb-web-server.lisp
@@ -9,10 +9,6 @@
              :initform (error "Web root not provided")
              :reader web-root
              :type pathname)
-   (dispatch-table :initarg :dispatch-table
-                   :initform (error "Dispatch table not provided")
-                   :accessor dispatch-table
-                   :type list)
    (port :initarg :port
          :initform (error "Port not provided")
          :reader port
@@ -23,16 +19,26 @@
    (server-handle :initform nil
                   :accessor server-handle)))
 
+(defmethod dispatch-table ((web imgdb-web-server))
+  (server-dispatch-table (server-handle web)))
+
+(defmethod set-dispatch-table ((web imgdb-web-server) new-val)
+  (setf (server-dispatch-table (server-handle web)) new-val))
+
+(defsetf dispatch-table set-dispatch-table)
+
 (defmethod start-imgdb-web-server ((web imgdb-web-server))
   (when (is-running? web)
     (error "imgdb web server is already running"))
+  (setf (server-handle web)
+        (start-server :dispatch-table '() :port (port web)))
   (setup-dispatch-table web)
-  (setf (server-handle web) (start-server :port (port web)))
   (setf (is-running? web) t))
 
 (defmethod stop-imgdb-web-server ((web imgdb-web-server))
   (unless (is-running? web)
     (error "imgdb web server is not running"))
+  (clear-dispatch-table web)
   (stop-server (server-handle web))
   (setf (server-handle web) nil)
   (setf (is-running? web) nil))
